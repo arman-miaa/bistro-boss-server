f8be9fffc86116ff7f943ef010d75b3cdb7bc102
Arman <mdarman365506@gmail.com>
2024-12-30 15:13:01 +0600

    added get api for menu and reviews

--- index.js
+++ index.js
@@ -27,15 +27,34 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+      await client.connect();
+      
+      const menuCollection = client.db('bistroDB').collection('menu');
+      const reviewCollection = client.db('bistroDB').collection('reviews');
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+      );
+      
+
+      app.get('/menu', async (req, res) => {
+          const result = await menuCollection.find().toArray();
+          res.send(result);
+})
+      app.get('/reviews', async (req, res) => {
+          const result = await reviewCollection.find().toArray();
+          res.send(result);
+})
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
