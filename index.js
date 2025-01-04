94b20bd05bd7eaf00acdb47b265b013d11c21677
Arman <mdarman365506@gmail.com>
2025-01-04 21:13:57 +0600

    create new collection and post api for user

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       
+      const userCollection = client.db('bistroDB').collection('users');
       const menuCollection = client.db('bistroDB').collection('menu');
       const reviewCollection = client.db('bistroDB').collection('reviews');
       const cartCollection = client.db('bistroDB').collection('carts');
@@ -40,7 +41,15 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
       );
       
-
+    // users related api
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+    
+    
+    
       app.get('/menu', async (req, res) => {
           const result = await menuCollection.find().toArray();
           res.send(result);
