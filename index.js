540e7ea29b79b7b700707d2cd1cd2155a6a54014
Arman <mdarman365506@gmail.com>
2025-01-05 16:38:13 +0600

    create patch api for makeing admin

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
       );
       
     // users related api
+
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       // insert email if user doesnt exists:
@@ -55,8 +62,29 @@ async function run() {
       res.send(result);
     })
     
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+        
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
     
     
+    // menu related api
       app.get('/menu', async (req, res) => {
           const result = await menuCollection.find().toArray();
           res.send(result);
